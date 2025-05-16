8cb5c7d1e3931f5b972a2e241dcbf3349a74c7eb
YZRay <yao19991119@gmail.com>
2025-05-16 18:04:05 +0800

    feat: add countdownTimer

---
+++ src/components/CountdownTimer.jsx
@@ -0,0 +1,57 @@
+"use client";
+import { useState, useEffect } from "react";
+
+function CountdownTimer({ initialTime }) {
+  const [timeLeft, setTimeLeft] = useState(initialTime);
+
+  useEffect(() => {
+    if (timeLeft === 0) return;
+
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime <= 1) {
+          clearInterval(timer);
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [timeLeft]);
+
+  const resetTimer = () => {
+    setTimeLeft(initialTime);
+  };
+
+  const formatTime = (seconds) => {
+    const days = Math.floor(seconds / (24 * 3600));
+    const hours = Math.floor((seconds % (24 * 3600)) / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
+    if (days > 0) {
+      return `${days} 天 ${hours} 時 ${minutes} 分 ${secs} 秒`;
+    }
+    if (hours > 0) {
+      return `${hours} 時 ${minutes} 分 ${secs} 秒`;
+    }
+    if (minutes > 0) {
+      return `${minutes} 分 ${secs} 秒`;
+    }
+    if (secs > 0) {
+      return `${secs} 秒`;
+    }
+    return `${days} 天 ${hours} 時 ${minutes} 分 ${secs} 秒`;
+  };
+
+  return (
+    <div>
+      <h1>剩餘時間：{formatTime(timeLeft)}</h1>
+      <button type="button" onClick={resetTimer} className="cursor-pointer">
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default CountdownTimer;
